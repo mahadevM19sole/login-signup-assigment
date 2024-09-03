a67b487ae0d45b46f56f120bd5b5618e5fa03d2d
Mahadev Sole <mahadevsole1@gmail.com>
2024-09-03 21:52:33 +0530

    adding first project

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+const EmployeeMdel=require('./models/Employee')
+
+const app=express()
+app.use(express.json())
+app.use(cors())
+
+
+
+mongoose.connect("mongodb://localhost:27017/employee")
+
+app.post('/register', (req,res)=>{
+    EmployeeMdel.create(req.body)
+    .then(employees=>res.json(employees))
+    .catch(err=>res.json(err))
+
+})
+
+app.listen(3001,()=>{
+    console.log("server is running")
+})
